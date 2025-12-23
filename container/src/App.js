5bb1bd40670bf28cccb995492dd4dcdf76b70eef
Shaikh Noman <shaikhmohdnoman612@gmail.com>
2025-12-23 23:01:45 +0530

    Initial Setup for container MFE completed

---
+++ container/src/App.js
@@ -0,0 +1,12 @@
+import { StylesProvider } from "@material-ui/core/styles";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from "react";
+const App = () => {
+  return (
+    <div>
+      <h1>This is Container</h1>
+    </div>
+  );
+};
+
+export default App;
