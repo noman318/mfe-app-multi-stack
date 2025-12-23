106ff6492e3e8abff9232f0ea14cb33050e5f603
Shaikh Noman <shaikhmohdnoman612@gmail.com>
2025-12-23 22:51:40 +0530

    Marketing app MFE created

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
+import App from "./App";
+import React from "react";
 
 // Mount function to start up the app
 const mount = (el) => {
-  ReactDOM.render(<h1>Hi there!</h1>, el);
+  ReactDOM.render(<App />, el);
 };
 
 // If we are in development and in isolation,
 // call mount immediately
-if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
     mount(devRoot);
